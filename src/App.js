bcc140286e814a361f2b67bb0da2e30fd9e5f306
dlagmltjd17 <dlagmltjd17@kakao.com>
2022-04-21 21:18:34 +0900

    간단한 리액트 앱 작성해보기

--- src/App.js
+++ src/App.js
@@ -1,62 +1,32 @@
-import PropTypes from "prop-types";
-
-function Food({name, picture}) {
-  return (
-    <div>
-      <h2>I like {name} </h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
+function Iphone({name,picture})
+{
+  return <h1> I used {name}<br></br><img src={picture}></img></h1>;
 }
 
-const foodILike = [
+const iPhoneIUsed = [
   {
-    id: 1,
-    name: 'Kimchi',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Korean_cuisine-Kimchi-08.jpg/1280px-Korean_cuisine-Kimchi-08.jpg',
-    rating: 5
+    name: 'iPhone 3GS',
+    image: 'http://www.madboxpc.com/wp-content/gallery/iphone3gs_hardware/photos-hardware-02-20090608.jpg'
   },
   {
-    id: 2,
-    name: 'Samgyeopsal',
-    image: 'https://blog.kakaocdn.net/dn/N1DaJ/btrxIMQ3Qkn/QS3dSXwBaAKeuUIjRhnKBK/img.jpg',
-    rating: 4.9
+    name: 'iPhone 6s',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fblog.trustico.com%2Fwp-content%2Fuploads%2F2017%2F01%2F6s.jpg&f=1&nofb=1'
   },
   {
-    id: 3,
-    name: 'Bibimbap',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F5%2F5c%2FBibimbap_(8111593238).jpg%2F180px-Bibimbap_(8111593238).jpg&f=1&nofb=1',
-    rating: 4.8
+    name: 'iPhone XR',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftheallapps.com%2Fwp-content%2Fuploads%2FiPhone-Xr-Color.jpg&f=1&nofb=1'
   },
   {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F24346643560B93B729&f=1&nofb=1',
-    rating: 4.5
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F182311514DA6AFEC19&f=1&nofb=1',
-    rating: 5
+    name: 'iPhone SE (3rd Gen.)',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.macheat.com%2Fwp-content%2Fuploads%2F2021%2F03%2F2022-iphone-se-to-launch-with-the-same-iphone-8-inspired-design-5g-532307-2.jpg&f=1&nofb=1'
   }
 ];
 
-const renderFood = dish => <Food name={dish.name} picture={dish.image} />
-
 function App() {
-  return (
+  return(
     <div>
-      {foodILike.map(dish =>
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />)}
+      {iPhoneIUsed.map(dish => <Iphone key={dish.name} name={dish.name} picture={dish.image}/>)}
     </div>
-    );
+  );
 }
-
-Food.PropTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
 export default App;
